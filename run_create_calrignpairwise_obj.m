f9be7d36f6675f1a0baeba8937fd4c0056cd59db
kwaki <kwaki@hhmi>
2023-08-17 04:03:12 -0400

    updating

--- run_create_calrignpairwise_obj.m
+++ run_create_calrignpairwise_obj.m
@@ -1,12 +1,15 @@
 %base_out = '/groups/branson/bransonlab/kwaki/ForceData/avian3dstuff/calibration/20220726/';
 %base_out = '/groups/branson/bransonlab/kwaki/ForceData/avian3dstuff/calibration/20220726_bigvideo';
-base_out = '/groups/branson/bransonlab/kwaki/ForceData/avian3dstuff/calibration/20220726_bigvideo_test';
+%base_out = '/groups/branson/bransonlab/kwaki/ForceData/avian3dstuff/calibration/20220726_bigvideo_test';
+%base_out = '/groups/branson/bransonlab/kwaki/ForceData/avian3dstuff/calibration/20220913_stereo_test';
+%base_out = '/groups/branson/bransonlab/kwaki/ForceData/avian3dstuff/calibration/20220913_stereo_test_copy';
+base_out = '/groups/branson/bransonlab/kwaki/ForceData/avian3dstuff/calibration/20221011';
 
 calib_filenames = { ...
     fullfile(base_out, 'cam_01_opencv.mat'), ...
     fullfile(base_out, 'cam_02_opencv.mat'), ...
     fullfile(base_out, 'cam_12_opencv.mat') ...
 };
-outname = fullfile(base_out, 'multi_cam_calib.mat');
+outname = fullfile(base_out, 'multi_cam_calib_updated_refactor.mat');
 
-create_calrignpairwise(outname, calib_filenames);
+create_calrignpairwise(outname, calib_filenames);   
