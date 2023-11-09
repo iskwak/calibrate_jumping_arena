293154660afadf6e271b0ee71fb02036036a67cc
kwaki <kwaki@hhmi>
2023-11-09 15:16:20 -0500

    adding example json files

--- run_docker.sh
+++ run_docker.sh
@@ -1,9 +1,6 @@
 # Expose the X server on the host.
 sudo xhost +local:root
 
-# --rm: Make the container ephemeral (delete on exit).
-# -it: Interactive TTY.
-# --gpus all: Expose all GPUs to the container.
 docker run \
   --rm \
   -it \
@@ -14,18 +11,5 @@ docker run \
   --user $(id -u):$(id -g) \
   --volume $(pwd):/root \
   --volume /nrs/branson/kwaki/jumping_data:/nrsdata \
-  --volume $(pwd):/workspace ikwak/calibrate \
+  --volume $(pwd):/workspace iskwak/calibrate \
   bash
-# docker run \
-#   --rm \
-#   -it \
-#   --gpus all \
-#   -v /tmp/.X11-unix:/tmp/.X11-unix \
-#   -e DISPLAY=$DISPLAY \
-#   -e QT_X11_NO_MITSHM=1 \
-#   -e MPLCONFIGDIR=/workspace/mplcache \
-#   --user $(id -u):$(id -g) \
-#   --volume $(pwd):/root \
-#   --volume /nrs/branson/kwaki/jumping_data:/nrsdata \
-#   --volume $(pwd):/workspace kwaki/cudagl \
-#   bash
